3564853f426532f861c41118b4fb494b506e5e0e
rodyrobles <rodyrob@gmail.com>
2017-06-07 17:48:54 -0500

    getNewQuote function assigned to the button

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,36 @@
 $(document).ready(function() {
-	function getNewQuote() {
-		$.ajax({
-			url:'https://api.forismatic.com/api/1.0/',
-			jsonp: 'jsonp', // added to request data from a different domain
-			dataType: 'jsonp',// added to request data from a different domain
-			data: {
-			method: 'getQuote',
-			format: 'jsonp', //instead of json
-			lang: 'en'
-			},
-			success: function(response) {
-				console.log(response/*.quoteText*/) //commented to see all data
-			}
-		});
-		}
-	getNewQuote();
+    var quote;
+    var author;
+
+    function getNewQuote() {
+        $.ajax({
+            url: 'https://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp', // added to request data from a different domain
+            dataType: 'jsonp', // added to request data from a different domain
+            data: {
+                method: 'getQuote',
+                format: 'jsonp', //instead of json
+                lang: 'en'
+            },
+            success: function(response) {
+                quote = response.quoteText;
+                author = response.quoteAuthor;
+                $('#quote').text(quote);
+                if (author) {
+                    $('#author').text('said by ' + author);
+                } else {
+                    $('#author').text('unknown');
+                }
+            }
+        });
+    }
+    $('.getQuote').on('click', function() {
+        getNewQuote();
+    });
 });
+
+
+// console.log(response.quoteText) //commented to see all data
 
 
 /*
